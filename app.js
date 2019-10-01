c1c4fa29fa9c1f2485e2e9c5164094d79b845c7b
BenjoNi123 <benjamin.kukuljac@hotmail.com>
2019-10-02 01:13:13 +0200

    mini calc

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 function calculate() {
    var n1 = Number(document.getElementById("numb1").value);
    var n2 = Number(document.getElementById("numb2").value);
-   sum= n1 + n2;
+   //var opt =document.getElementById("sel").value;
+   if(document.getElementById('sel').value == "suma") {
+       sum = n1 + n2;
+}
+   else if (document.getElementById('sel').value == "minus") {
+       sum = n1 - n2;
+   } 
+   else if (document.getElementById('sel').value == "multiply") {
+       sum = n1 * n2;
+   }
+   else if (document.getElementById('sel').value == "divide") {
+       sum = n1 / n2;
+   }
    var Result = document.getElementById("Result").innerHTML = sum;
 };
