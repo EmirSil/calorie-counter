e8c6beb311111fa1d9dbace6889d763b66795f3f
Emir Silajdzic <emir.silajdzic.it@gmail.com>
2019-10-15 12:22:06 +0200

    Add comments to FPage.vue

--- caloriecounter/src/components/FPage.vue
+++ caloriecounter/src/components/FPage.vue
@@ -1,15 +1,39 @@
 <template>
-
-<div title> <h2> THIS IS OUR HOME PAGE </h2>
-<h3>Please choose to Log In or Register to our Calorie Counter</h3>
-</div>
-
-
-    </template>
-
-    <script>
+    <div> 
+        <h2>Name: {{name}}</h2>
+        <h2>Age: {{age}}</h2>
+    </div>
+</template>
+<script>
     export default {
-        name: "FPage"
+        //Here you write the name of the component, 
+        //ussualy it has the same name as the component file
+        //FPage...
+        name: "FPage",
+
+        //This is where you create your data variables
+        //These data variables you will be able to use everywhere
+        //in the script tags (line 7 to 41) you call these variables with this.VARIABLE_NAME
+        //in the html (line 1 to 6) you call these variables with {{VARIABLE_NAME}}
+        data() {
+            return {
+                name: '',
+                age: 0
+            }
+        },
+
+        //This is where you write your function
+        //since the "functions" are in an object
+        //they are called methods
+        methods: {
+
+            //This is how you write a method
+            //literally the same as a function
+            //we just drop the name "function" in the beginning 
+            showUser() {
+                //jsCode
+            }
+        }
     }
 </script>
 
