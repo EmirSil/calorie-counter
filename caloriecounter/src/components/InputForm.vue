da3bbe5c7b618e51d4224ad21a4b19ec9dabe764
BenjoNi123 <benjamin.kukuljac@hotmail.com>
2019-10-25 13:48:21 +0200

    Add pushing into array of objects

--- caloriecounter/src/components/InputForm.vue
+++ caloriecounter/src/components/InputForm.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <md-field>
-            <md-input type="text" placeholder="Enter your Name" v-model="name"></md-input>
-            <md-input type="text" placeholder="Enter Your Nickname" v-model="nickname"></md-input>
-            <md-input type="text" placeholder="Enter your Surname" v-model="surname"></md-input>
+            <md-input type="text" placeholder="Enter your Name" v-model="newUser.name"></md-input>
+            <md-input type="text" placeholder="Enter Your Nickname" v-model="newUser.nickname"></md-input>
+            <md-input type="text" placeholder="Enter your Surname" v-model="newUser.surname"></md-input>
             <md-button class="md-raised md-primary" @click.prevent="fillArray">Submit</md-button>
         </md-field>
         
@@ -13,25 +13,36 @@
 <script>
 export default {
     data() {
+        let myArray = {
+                    nameList:[],
+                    nicknameList:[],
+                    surnameList:[],
+                }
         return {
             name: null,
             surname: null,
             nickname: null,
-            nameList:[],
-            nicknameList:[],
-            surnameList:[],
+            newUser: {},
+            users: []
+                
 
             
         }
     },
     methods: {
         fillArray() {
-            this.nameList.push(this.name),
+            this.users.push({
+                name: this.newUser.name,
+                surname: this.newUser.surname,
+                nickname: this.newUser.nickname,
+            }),
+            this.newUser.name='',
+            this.newUser.surname='',
+            this.newUser.nickname='' //WE ARE PUSHING INTO ARRAY OF OBJECTS
+            /*this.nameList.push(this.name)
             this.nicknameList.push(this.nickname),
             this.surnameList.push(this.surname),
-            this.name='',
-            this.surname='',
-            this.nickname=''
+            */    //WE ARE PUSHING INTO ARRAY
             },
         
     }    
